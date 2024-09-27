a21c4ff9668db33be08e970d3afd1da6efaa521e
owenbris825 <owenbris825@SDS-L-9982Y93>
2024-09-27 10:19:56 -0400

    Add project files.

---
+++ cashRegister/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace cashRegister
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.freakBobCalls = new System.Windows.Forms.TextBox();
+            this.diddyPartyTickets = new System.Windows.Forms.TextBox();
+            this.meatSlabs = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.subtotalLabel = new System.Windows.Forms.Label();
+            this.taxLabel = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tenderedBox = new System.Windows.Forms.TextBox();
+            this.calculateChangeButton = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.changeLabel = new System.Windows.Forms.Label();
+            this.printButton = new System.Windows.Forms.Button();
+            this.newOrderButton = new System.Windows.Forms.Button();
+            this.receiptLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(280, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Owen\'s Freaky Store";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(177, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Number Of Freak Bobs Calls";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(191, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Number Of Diddy Party Tickets";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(205, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Number Of John Pork Meat Slabs";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(18, 155);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(309, 23);
+            this.calculateButton.TabIndex = 4;
+            this.calculateButton.Text = "Calculate Total";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // freakBobCalls
+            // 
+            this.freakBobCalls.Location = new System.Drawing.Point(227, 58);
+            this.freakBobCalls.Name = "freakBobCalls";
+            this.freakBobCalls.Size = new System.Drawing.Size(100, 22);
+            this.freakBobCalls.TabIndex = 5;
+            this.freakBobCalls.TextChanged += new System.EventHandler(this.freakBobCalls_TextChanged);
+            // 
+            // diddyPartyTickets
+            // 
+            this.diddyPartyTickets.Location = new System.Drawing.Point(227, 92);
+            this.diddyPartyTickets.Name = "diddyPartyTickets";
+            this.diddyPartyTickets.Size = new System.Drawing.Size(100, 22);
+            this.diddyPartyTickets.TabIndex = 6;
+            // 
+            // meatSlabs
+            // 
+            this.meatSlabs.Location = new System.Drawing.Point(227, 127);
+            this.meatSlabs.Name = "meatSlabs";
+            this.meatSlabs.Size = new System.Drawing.Size(100, 22);
+            this.meatSlabs.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 191);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Subtotal";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 222);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 16);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Tax";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(15, 254);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 16);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Total";
+            // 
+            // subtotalLabel
+            // 
+            this.subtotalLabel.AutoSize = true;
+            this.subtotalLabel.Location = new System.Drawing.Point(227, 191);
+            this.subtotalLabel.Name = "subtotalLabel";
+            this.subtotalLabel.Size = new System.Drawing.Size(16, 16);
+            this.subtotalLabel.TabIndex = 11;
+            this.subtotalLabel.Text = "...";
+            // 
+            // taxLabel
+            // 
+            this.taxLabel.AutoSize = true;
+            this.taxLabel.Location = new System.Drawing.Point(227, 222);
+            this.taxLabel.Name = "taxLabel";
+            this.taxLabel.Size = new System.Drawing.Size(16, 16);
+            this.taxLabel.TabIndex = 12;
+            this.taxLabel.Text = "...";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Location = new System.Drawing.Point(227, 254);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(16, 16);
+            this.totalLabel.TabIndex = 13;
+            this.totalLabel.Text = "...";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(60, 291);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(67, 16);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Tendered";
+            // 
+            // tenderedBox
+            // 
+            this.tenderedBox.Location = new System.Drawing.Point(227, 285);
+            this.tenderedBox.Name = "tenderedBox";
+            this.tenderedBox.Size = new System.Drawing.Size(100, 22);
+            this.tenderedBox.TabIndex = 15;
+            // 
+            // calculateChangeButton
+            // 
+            this.calculateChangeButton.Location = new System.Drawing.Point(18, 313);
+            this.calculateChangeButton.Name = "calculateChangeButton";
+            this.calculateChangeButton.Size = new System.Drawing.Size(309, 23);
+            this.calculateChangeButton.TabIndex = 16;
+            this.calculateChangeButton.Text = "Calculate Change";
+            this.calculateChangeButton.UseVisualStyleBackColor = true;
+            this.calculateChangeButton.Click += new System.EventHandler(this.calculateChangeButton_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(60, 350);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(54, 16);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Change";
+            // 
+            // changeLabel
+            // 
+            this.changeLabel.AutoSize = true;
+            this.changeLabel.Location = new System.Drawing.Point(227, 350);
+            this.changeLabel.Name = "changeLabel";
+            this.changeLabel.Size = new System.Drawing.Size(16, 16);
+            this.changeLabel.TabIndex = 18;
+            this.changeLabel.Text = "...";
+            // 
+            // printButton
+            // 
+            this.printButton.Location = new System.Drawing.Point(15, 379);
+            this.printButton.Name = "printButton";
+            this.printButton.Size = new System.Drawing.Size(312, 23);
+            this.printButton.TabIndex = 19;
+            this.printButton.Text = "Print Receipt";
+            this.printButton.UseVisualStyleBackColor = true;
+            this.printButton.Click += new System.EventHandler(this.printButton_Click);
+            // 
+            // newOrderButton
+            // 
+            this.newOrderButton.Location = new System.Drawing.Point(368, 379);
+            this.newOrderButton.Name = "newOrderButton";
+            this.newOrderButton.Size = new System.Drawing.Size(316, 23);
+            this.newOrderButton.TabIndex = 20;
+            this.newOrderButton.Text = "New Order";
+            this.newOrderButton.UseVisualStyleBackColor = true;
+            this.newOrderButton.Click += new System.EventHandler(this.newOrderButton_Click);
+            // 
+            // receiptLabel
+            // 
+            this.receiptLabel.BackColor = System.Drawing.Color.White;
+            this.receiptLabel.Location = new System.Drawing.Point(368, 58);
+            this.receiptLabel.Name = "receiptLabel";
+            this.receiptLabel.Size = new System.Drawing.Size(316, 278);
+            this.receiptLabel.TabIndex = 21;
+            this.receiptLabel.Text = "Owen\'s Freaky Store Purchase...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(696, 418);
+            this.Controls.Add(this.receiptLabel);
+            this.Controls.Add(this.newOrderButton);
+            this.Controls.Add(this.printButton);
+            this.Controls.Add(this.changeLabel);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.calculateChangeButton);
+            this.Controls.Add(this.tenderedBox);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.taxLabel);
+            this.Controls.Add(this.subtotalLabel);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.meatSlabs);
+            this.Controls.Add(this.diddyPartyTickets);
+            this.Controls.Add(this.freakBobCalls);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.TextBox freakBobCalls;
+        private System.Windows.Forms.TextBox diddyPartyTickets;
+        private System.Windows.Forms.TextBox meatSlabs;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label subtotalLabel;
+        private System.Windows.Forms.Label taxLabel;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox tenderedBox;
+        private System.Windows.Forms.Button calculateChangeButton;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label changeLabel;
+        private System.Windows.Forms.Button printButton;
+        private System.Windows.Forms.Button newOrderButton;
+        private System.Windows.Forms.Label receiptLabel;
+    }
+}
+
---
+++ cashRegister/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cashRegister
+{
+    public partial class Form1 : Form
+    {
+        //global variables
+        double freakCalls;
+        double calls;
+        double diddyTickets;
+        double tickets;
+        double johnMeat;
+        double slabs;
+        double subtotal;
+        double tax;
+        double total;
+        double tendered;
+        double change;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void freakBobCalls_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            calls = Convert.ToDouble(freakBobCalls.Text);
+            freakCalls = 6.99;
+            
+
+            tickets = Convert.ToDouble(diddyPartyTickets.Text);
+            diddyTickets = 49.99;
+            
+
+            slabs = Convert.ToDouble(meatSlabs.Text);
+            johnMeat = 22.49;
+
+
+            subtotal = calls * freakCalls + tickets * diddyTickets + slabs * johnMeat;
+            subtotalLabel.Text = $"{subtotal.ToString("C")}";
+
+            tax = subtotal * 0.13;
+            taxLabel.Text = $"{tax.ToString("C")}";
+
+            total = subtotal + tax;
+            totalLabel.Text = $"{total.ToString("C")}";
+        }
+
+        private void calculateChangeButton_Click(object sender, EventArgs e)
+        {
+            tendered = Convert.ToDouble(tenderedBox.Text);
+            change = tendered - total;
+            changeLabel.Text = $"{change.ToString("C")}";
+        }
+
+        private void printButton_Click(object sender, EventArgs e)
+        {
+            receiptLabel.Text += $"\n{calls} Freak Bobs Calls....                               {calls * freakCalls}";
+            receiptLabel.Text += $"\n{tickets} Diddy Party Tickets....                          {tickets * diddyTickets}";
+            receiptLabel.Text += $"\n{slabs} John Pork Meat Slabs....                      {slabs * johnMeat}";
+
+            receiptLabel.Text += $"\nSubtotal....                                            {subtotal.ToString("C")}";
+            receiptLabel.Text += $"\nTax....                                                   {tax.ToString("C")}";
+            receiptLabel.Text += $"\nTotal....                                                 {total.ToString("C")}";
+            receiptLabel.Text += $"\nPayed....                                               {tendered.ToString("C")}";
+            receiptLabel.Text += $"\nChange....                                             {change.ToString("C")}";
+        }
+
+        private void newOrderButton_Click(object sender, EventArgs e)
+        {
+            receiptLabel.Text = "";
+            changeLabel.Text = "";
+            totalLabel.Text = "";
+            taxLabel.Text = "";
+            subtotalLabel.Text = "";
+            freakBobCalls.Text = "";
+            diddyPartyTickets.Text = "";
+            meatSlabs.Text = "";
+            tenderedBox.Text = "";
+            calls = 0;
+            tickets = 0;
+            slabs = 0;
+            subtotal = 0;
+            total = 0;
+            tax = 0;
+            change = 0;
+            tendered = 0;
+        }
+    }
+}
